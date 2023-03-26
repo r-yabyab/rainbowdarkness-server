436bade69ef93225a82245a0a8e6d275de905c98
Scruffy pc <cayabyabrr@yahoo.com>
2023-03-25 21:44:41 -0700

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 app.use('/api/rainbows', rainbowRoutes)
 
 
-app.get('/aineg', apiLimiter, async (req, res) => {
+app.get('/aineg', async (req, res) => {
     const completion = await openai.createChatCompletion({
         model:'gpt-3.5-turbo',
         messages:[
@@ -52,7 +52,7 @@ app.get('/aineg', apiLimiter, async (req, res) => {
     console.log(completion.data.choices[0].message)
 })
 
-app.get('/aipos', apiLimiter, async (req, res) => {
+app.get('/aipos', async (req, res) => {
     const completion = await openai.createChatCompletion({
         model:'gpt-3.5-turbo',
         messages:[
