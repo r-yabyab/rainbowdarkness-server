4be50b723c17e1c2f49696d6122afead7342b93b
Scruffy pc <cayabyabrr@yahoo.com>
2023-04-02 00:48:29 -0700

    cors memo

--- routes/rainbowMemoRoutes.js
+++ routes/rainbowMemoRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {postRainbowMemo} = require('../controllers/rainbowController')
+const {postRainbowMemo} = require('../controllers/rainbowMemoController')
 const router = express.Router()
 
 
