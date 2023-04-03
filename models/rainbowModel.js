890e6ec3704eee2014c2db16d6bc2d8dea93f3e0
Scruffy pc <cayabyabrr@yahoo.com>
2023-04-02 23:06:35 -0700

    commit message

--- models/rainbowModel.js
+++ models/rainbowModel.js
@@ -14,6 +14,6 @@ const rainbowSchema = new Schema ({
 //         type: String,
 //         required: true
 //     }
-// }, {timestamps: true})
+// }, {timestamps: true}) //
 
 module.exports = mongoose.model("Rainbow", rainbowSchema)
