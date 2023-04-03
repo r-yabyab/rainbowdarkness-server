b6bcd77d8449261d0d2871b3007585c6f2786b55
Scruffy pc <cayabyabrr@yahoo.com>
2023-04-02 21:16:27 -0700

    added jwt packages, testing

--- routes/rainbowMemoRoutes.js
+++ routes/rainbowMemoRoutes.js
@@ -4,16 +4,17 @@ const rateLimit = require('express-rate-limit')
 // const { checkJwt } = require('../middleware/auth')
 const router = express.Router()
 
-const apiLimiter = rateLimit({
-    windowMs: 86400000, //24 hours is  86400000
-    max: 1,
-    standardHeaders: true,
-    legacyHeaders: false,
-    store: new rateLimit.MemoryStore(),
-})
+// const apiLimiter = rateLimit({
+//     windowMs: 86400000, //24 hours is  86400000
+//     max: 1,
+//     standardHeaders: true,
+//     legacyHeaders: false,
+//     store: new rateLimit.MemoryStore(),
+// })
 
 // router.post('/', checkJwt, apiLimiter, postRainbowMemo)
-router.post('/', apiLimiter, postRainbowMemo)
+router.post('/', postRainbowMemo)
+// router.post('/', apiLimiter, postRainbowMemo)
 router.get('/single/:id', getSingleRainbowMemo)
 router.get('/last', getLastMemo)
 
