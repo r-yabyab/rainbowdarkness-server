6d2ce0704f1b3cee12b24ed3099f605b0313721d
Scruffy pc <cayabyabrr@yahoo.com>
2023-04-02 23:43:26 -0700

    cors error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('express-jwt')
-const jwks = require('jwks-rsa')
+// const jwt = require('express-jwt')
+// const jwks = require('jwks-rsa')
 
-const authConfig = {
-    domain: 'https://dev-bxpbdydalm6tmklv.us.auth0.com',
-    audience: 'https://www.rainbowdarkness-api.com',
-}
+// const authConfig = {
+//     domain: 'https://dev-bxpbdydalm6tmklv.us.auth0.com',
+//     audience: 'https://www.rainbowdarkness-api.com',
+// }
 
-const checkJwt = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://dev-bxpbdydalm6tmklv.us.auth0.com/.well-known/jwks.json`
-    }),
+// const checkJwt = jwt({
+//     secret: jwks.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: `https://dev-bxpbdydalm6tmklv.us.auth0.com/.well-known/jwks.json`
+//     }),
 
-    audience: authConfig.audience,
-    issuer: `https://${authConfig.domain}/`,
-    algorithms: ['RS256']
-})
+//     audience: authConfig.audience,
+//     issuer: `https://${authConfig.domain}/`,
+//     algorithms: ['RS256']
+// })
 
 // module.exports = {checkJwt};
