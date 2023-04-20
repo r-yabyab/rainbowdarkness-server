baef4254bd82eb687863af9bf43a79ad749e0035
Scruffy pc <cayabyabrr@yahoo.com>
2023-04-20 00:49:20 -0700

    comments on schema

--- models/rainbowModel.js
+++ models/rainbowModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const rainbowSchema = new Schema ({
+    // if user submits decimal, MongoDB stores as a Double
+    // if whole number, Int32
     number: {
         type: Number,
         required: true
