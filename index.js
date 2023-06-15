cb96f0aa56e881d1a6977eed68236db2d75a93e3
Scruffy pc <cayabyabrr@yahoo.com>
2023-06-15 15:45:45 -0700

    removed aws-sdk

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const AWS = require('aws-sdk')
-AWS.config.region = 'us-west-2b';
+// const AWS = require('aws-sdk')
+// AWS.config.region = 'us-west-2b';
 const cors = require('cors')
 const rainbowRoutes = require('./routes/rainbowRoutes')
 const rainbowMemoRoutes = require('./routes/rainbowMemoRoutes')
